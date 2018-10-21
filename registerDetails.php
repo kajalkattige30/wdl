2ba01427fbc1df39720d84b214eb6df9503970a3
Janhavi <janhavi.dubule16@siesgst.ac.in>
2018-10-21 19:27:12 +0530

    registerform working!

--- registerDetails.php
+++ registerDetails.php
@@ -1,6 +1,7 @@
-<<?php
+<?php
 	if($_POST["submit"]=="submit")
 	{
+		//$uname = "mm";
 		session_start();
 		$uname = $_SESSION['username'];
 		$fname = $_POST["fname"];
@@ -9,12 +10,12 @@
 		$email = $_POST["email"];
 		$sex = $_POST["sex"];
 		$address = $_POST["add"];
-		$dbcon = mysqli_connect("localhost","root","","shopdb");
+		$dbcon = mysqli_connect("localhost","root","","shopDB");
 		echo "$uname";
 		echo "$fname";
 		//name=localStorage.getItem("firstname");
-		$s = "insert into customer_info(fname,lname,contact,email,sex,address) values('$uname','$lname',$mobile,'$email','$sex','$add') ";
-		mysqli_query($dbcon,$s);
+		$s = "insert into customer_info(uname,fname,lname,contact,email,sex,address) values('$uname','$fname','$lname',$mobile,'$email','$sex','$address') ";
+		$q = mysqli_query($dbcon,$s);
 		echo "registeration Completed";
 		//header('location:custprofile.html');
 	} 
--- registration.php
+++ registration.php
@@ -1,11 +1,14 @@
 <?php
+$uname = "";
+$con = mysqli_connect('localhost','root',"","shopdb");
 if($_POST["register"]=="Register")
 {
 session_start();
 header('location:rform.php');
-$con = mysqli_connect('localhost','root',"","shopdb");
+//$con = mysqli_connect('localhost','root',"","shopdb");
 //mysqli_select_db($con, 'shopdb');
 $name = $_POST['user'];
+$uname = $name;
 $pass = $_POST['pass'];
 $_SESSION['username'] = $name;
 $s = "select * from customers where Username = '$name'";
@@ -22,4 +25,24 @@ else{
 	header("location:registerpage.html");
 }
 }
+if($_POST["submit"]=="submit")
+	{
+		echo "entered";
+		//session_start();
+		//$uname = $_SESSION['username'];
+		$fname = $_POST["fname"];
+		$lname = $_POST["lname"];
+		$mobile = $_POST["mobile"];
+		$email = $_POST["email"];
+		$sex = $_POST["sex"];
+		$address = $_POST["add"];
+		//$dbcon = mysqli_connect("localhost","root","","shopDB");
+		echo "$uname";
+		echo "$fname";
+		//name=localStorage.getItem("firstname");
+		$sc = "insert into customer_info(fname,lname,contact,email,sex,address) values('$uname','$lname',$mobile,'$email','$sex','$address') ";
+		$q = mysqli_query($con,$sc);
+		echo "registeration Completed";
+		//header('location:custprofile.html');
+	}
 ?>
